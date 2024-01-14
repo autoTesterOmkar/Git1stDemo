ce7ce1676359a0113f4ce6501f38698be89ca27f
Omkar <opawar449@gmail.com>
2024-01-14 13:17:56 +0530

    first commit

---
+++ src/Test.java
@@ -0,0 +1,20 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int k=1;
+      
+       
+       for(int i=4 ;i>0; i--) {
+    	   
+    	   for (int j=1; j<=5-i; j++) {
+    		   System.out.print(j);
+    		   System.out.print("\t");
+    		   
+    	   }
+    	   System.out.println("");
+       }
+	}
+
+}
